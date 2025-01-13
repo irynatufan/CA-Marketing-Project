88ff39b823caabe342af83a0338ac4091360e2ba
Iryna Tufan <149471365+irynatufan@users.noreply.github.com>
2025-01-13 21:01:29 +0300

    Delete 2_Front-End Testing/my_key.py

--- 2_Front-End Testing/my_key.py
+++
@@ -1,4 +0,0 @@
-# "Please past here your access key from your BrowserStack account to run the script above"
-
-BROWSERSTACK_USERNAME = "your_BROWSERSTACK_USERNAME"
-BROWSERSTACK_ACCESS_KEY = "your_BROWSERSTACK_ACCESS_KEY"
